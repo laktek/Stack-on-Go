0a8e76c5345f17f1afea1519e1ba08606a3899cc
Lakshan Perera <lakshan@web2media.net>
2012-02-04 17:52:21 +0530

    added the example file

---
+++ examples/go_faq.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "time"
+import "github.com/laktek/stack-on-go"
+
+func main() {
+	fmt.Printf("Frequently Asked Questions on Go:\n")
+
+	session := stackongo.NewSession("stackoverflow")
+	questions, err := session.FAQForTags([]string{"Go"}, map[string]string{})
+
+	if err != nil {
+		fmt.Printf(err.String())
+	}
+
+  for _, question := range questions.Items {
+		fmt.Printf("%v\n", question.Title)
+		fmt.Printf("Asked By: %v on %v\n", question.Owner.Display_name, time.SecondsToUTC(question.Creation_date))
+		fmt.Printf("Link: %v\n\n", question.Link)
+	}
+}
